a0a39c46003d9e850623837c5e5682b085ce63ed
Marcelo Duarte Trevisani <marcelo.trevisani@xfel.eu>
2018-08-02 10:02:58 +0200

    add exist.home

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,12 @@
 mkdir -p "${PREFIX}/share/existdb"
 mkdir -p "${PREFIX}/share/existdb/var/webapp/WEB-INF/data"
 
-echo "INSTALL_PATH=${PREFIX}/share/existdb
-dataDir=${PREFIX}/share/existdb/var/webapp/WEB-INF/data
+echo "EXIST_HOME=${PREFIX}/share/existdb
+INSTALL_PATH=${PREFIX}/share/existdb
 MAX_MEMORY=1024
-cacheSize=128
+CACHE_MEM=256
+EXIST_DATA_DIR=${PREFIX}/share/existdb/var/webapp/WEB-INF/data
+EXIST_LOGS_DIR=${PREFIX}/share/existdb/var/webapp/WEB-INF/log
 adminPasswd=admin" > "${RECIPE_DIR}/options.txt"
 
 # Install the database
