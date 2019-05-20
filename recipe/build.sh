e0be7ce1401964f1671f5bd7d23c8ae25cbd3aa6
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2019-05-20 13:47:55 +0100

    Update build

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,9 @@ MAX_MEMORY=1024
 CACHE_MEM=256
 EXIST_DATA_DIR=${PREFIX}/share/existdb/var/webapp/WEB-INF/data
 EXIST_LOGS_DIR=${PREFIX}/share/existdb/var/webapp/WEB-INF/log
-adminPasswd=admin" > "${RECIPE_DIR}/options.txt"
+adminPasswd=admin" > "${PREFIX}/share/existdb/var/options.txt"
+
+chmod 777 "${PREFIX}/share/existdb/var/options.txt"
 
 # Install the database
-java -jar "${SRC_DIR}/existdb.jar" -console -options "${RECIPE_DIR}/options.txt"
+java -jar "${SRC_DIR}/existdb.jar" -console -options "${PREFIX}/share/existdb/var/options.txt"
